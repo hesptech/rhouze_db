7d1b29dddbf91b719783cd96cb919bc871295ac6
oswaldo olea <o.olea@sintraconsulting.com>
2023-06-16 15:52:00 +0200

    Refactory: update Rhouze db

--- rhouze_bd.sql
+++ rhouze_bd.sql
@@ -1,9 +1,30 @@
+
 CREATE TABLE users
 (
-    id                int auto_increment PRIMARY KEY,
-    username          varchar(25)  NOT NULL,
+    id_user           int auto_increment PRIMARY KEY,
+    username          varchar(40)  NOT NULL,
     email             varchar(255) NOT NULL,
     password          varchar(255) NOT NULL,
-    active            tinyint(1)            DEFAULT 0,
-    activation_code   varchar(255) NOT NULL,
+    active            tinyint(1)   DEFAULT 0,
+    activation_code   int(4)       NOT NULL,
+);
+
+
+CREATE TABLE mls_guesses
+(
+    id_mls_guesse    int auto_increment PRIMARY KEY,
+    id_user           int PRIMARY KEY,
+    mlsNumber         varchar(20)  NOT NULL,
+    guess_price       int(20),     NOT NULL,
+    expiryDate        datetime     NOT NULL,    
+);
+
+
+CREATE TABLE favorites
+(
+    id_favorite    int auto_increment PRIMARY KEY,
+    id_user           int PRIMARY KEY,
+    mlsNumber         varchar(20)  NOT NULL,
+    guess_price       int(20),     NOT NULL,
+    expiryDate        datetime     NOT NULL,    
 );
