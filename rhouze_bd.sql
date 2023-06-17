176362d530f8a579b6c5935f9eeb9c0ceb279d0a
oswaldo olea <o.olea@sintraconsulting.com>
2023-06-17 15:48:54 +0200

    Refactor: dump to mySQL

--- rhouze_bd.sql
+++ rhouze_bd.sql
@@ -1,28 +1,30 @@
 
-CREATE TABLE users
+
+CREATE TABLE `users`
 (
-    id_user           int auto_increment PRIMARY KEY,
-    username          varchar(40)  NOT NULL,
-    email             varchar(255) NOT NULL,
-    password          varchar(255) NOT NULL,
-    active            tinyint(1)   DEFAULT 0,
-    activation_code   int(4)       NOT NULL,
+    `id_user`           int auto_increment PRIMARY KEY,
+    `username`          varchar(40)  NOT NULL,
+    `email`             varchar(255) NOT NULL,
+    `password`          varchar(255) NOT NULL,
+    `active`            tinyint(1)   DEFAULT 0,
+    `activation_code`   int(4)       NOT NULL
 );
 
 
-CREATE TABLE mls_guesses
+CREATE TABLE `mls_guesses`
 (
-    id_mls_guesse    int auto_increment PRIMARY KEY,
-    id_user           int PRIMARY KEY,
-    mlsNumber         varchar(20)  NOT NULL,
-    guess_price       int(20),     NOT NULL,
-    expiryDate        datetime     NOT NULL,    
+    `id_mls_guess`      int auto_increment PRIMARY KEY,
+    `id_user`           int,
+    `mlsNumber`         varchar(20)  NOT NULL,
+    `guess_price`       int(20)     NOT NULL,
+    `expiryDate`        datetime     NOT NULL   
 );
 
 
-CREATE TABLE favorites
+CREATE TABLE `favorites`
 (
-    id_favorite    int auto_increment PRIMARY KEY,
-    id_user           int PRIMARY KEY,
-    mlsNumber         varchar(20)  NOT NULL,    
+    `id_favorite`      int auto_increment PRIMARY KEY,
+    `id_user`          int,
+    `mlsNumber`        varchar(20)  NOT NULL 
 );
+
