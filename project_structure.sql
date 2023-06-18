8881128a538d8de850f8675cc69edf0bcd9c3e1d
oswaldo olea <olea.oswaldo@gmail.com>
2023-06-18 13:46:44 +0200

    Feat: proyect structure

---
+++ project_structure.sql
@@ -0,0 +1,35 @@
+├── config
+|  ├── app.php
+|  └── database.php
+├── public
+|  ├── index.php
+|  ├── login.php
+|  ├── logout.phpS
+|  └── register.php
+└── src
+    ├── auth.php
+    ├── bootstrap.php
+    ├── inc
+    |  ├── footer.php
+    |  └── header.php
+    ├── libs
+    |  ├── connection.php
+    |  ├── filter.php
+    |  ├── flash.php
+    |  ├── helpers.php
+    |  ├── sanitization.php
+    |  └── validation.php
+    ├── login.php
+    └── register.php
+
+
+https://www.phptutorial.net/php-tutorial/php-email-verification/
+
+
+
+{
+    "mlsNumber": "example_C6129260",
+    "guess_price": "10500100"
+    "expiryDate": "2023-09-01"
+}
+
