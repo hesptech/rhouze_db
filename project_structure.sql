b6024cf9e422c1162fd342173d974a7958fde142
hesptech <olea.oswaldo@gmail.com>
2023-11-06 19:05:04 +0100

    Added jsons, structure txt

--- project_structure.sql
+++ project_structure.sql
@@ -27,37 +27,6 @@ https://www.phptutorial.net/php-tutorial/php-email-verification/
 
 
 
-├── screens
-|  ├── game_guess_screen.dart
-|  ├── game_guess_price_screen.dart
-|  └── game_guess_list_screen.dart
-└── module
-    └── game
-       ├── game_guess_box.dart
-       ├── game_guess_card.dart
-       └── game_guess_card_components.dart
-
-
-├── screens
-| ├── game_guess_screen.dart
-| ├── game_guess_price_screen.dart
-| └── game_guess_list_screen.dart
-└── module
-  └── game_guess
-    ├── game_guess_price
-    | ├── game_guess_price_box.dart
-    | ├── game_guess_price_submit.dart
-    | └── game_guess_price.dart
-    └── game_guess_list
-      ├── game_guess_list_box.dart
-      ├── game_guess_list_component.dart
-      ├── game_guess_list.dart
-      └── game_guess_card
-        ├── game_guess_card_box.dart
-        ├── game_guess_card_component.dart
-        └── game_guess_card.dart
-
-
 
 
 
